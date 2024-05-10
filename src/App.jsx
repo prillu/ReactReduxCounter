d0aee24db56305cb1b7922b33a7c6c7701a0d611
prillu <prilnaunnimol@gmail.com>
2024-05-10 10:12:13 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './Components/Counter'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{height:'100vh'}}
+      className="d-flex justify-content-center align-items-center w-100">
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset,incrementByAmount } from '../redux/counterSlice'
+
+const Counter = () => {
+  const[amount,setAmount]= useState(0)
+
+  const dispatch = useDispatch()
+  const {count} = useSelector(state => state.counterReducer)
+  const handleIncrement=()=>{
+if(amount){
+  dispatch(incrementByAmount(parseInt(amount)))
+}else{
+  alert('Please enter amount to proceed')
+}
+
+  }
+  return (
+
+    <div>
+      <h1 className='text-center'>Counter App</h1>
+      <div style={{width:'800px'}} className="border p-5 rounded mt-5 ">
+        <h1 style={{fontSize:'80px'}} className="text-center">{count}</h1>
+        <div className="d-flex justify-content-around align-items-center mt-3 w-100">
+<button onClick={()=>dispatch(decrement())} className="btn btn-warning">DECREMENT</button>
+<button onClick={()=>dispatch(reset())} className="btn btn-danger">RESET</button>
+<button onClick={()=>dispatch(increment())} className="btn btn-success">INCREMENT</button>
+
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center mt-5 w-100">
+            <input onChange={e=>setAmount(e.target.value)} type='text' className="form-control" placeholder='Incremented Counter Amount'/>
+            <button onClick={handleIncrement} className="btn btn-primary ms-2">INCREMENT BY AMOUNT</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
